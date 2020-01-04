7553eee78cda653658c3db5bc351a51f191b9678
Ray-Tw <54581745+Ray-Tw@users.noreply.github.com>
2020-01-04 21:49:10 +0800

    Add files via upload

--- HDFS.py
+++ HDFS.py
@@ -4,6 +4,7 @@ import cv2
 import os 
 import numpy as np
 import threading
+import time
 import socket
 import warnings
 warnings.simplefilter("ignore", DeprecationWarning) #fromstring is deprecated
@@ -27,7 +28,7 @@ def service(client_socket, file_name, client_number):
     directory = '\HW2\frame'+str(client_number)
     f_handler=open(str(client_number)+ '_' + str(file_name)+ '.txt', 'a')
     __console__ = sys.stdout
-    while( c < num_frame ): #frame數
+    while( c < num_frame ): #total num of frame
         length = int(client_socket.recv(16).decode('utf-8'))
 
         #print('frame size : '+ str(length)) #277090
@@ -35,7 +36,7 @@ def service(client_socket, file_name, client_number):
         frame = np.array(recvall(client_socket, length), 'uint8')
         
 
-        #剛收到再傳HDFS
+        #send to HDFS
         strdata=frame.tostring()
 
         
@@ -53,7 +54,7 @@ def service(client_socket, file_name, client_number):
         cv2.imwrite( str(c)+'.jpg' , img_np)
 
         if int(detect) == 1:
-            sys.stdout=f_handler  #print到file檔
+            sys.stdout=f_handler  #print to file.txt
             print(str(c)) #racing condition
         c += 1
         
@@ -75,14 +76,14 @@ if __name__ == '__main__':
 
         client_socket,addr = hdfs_socket.accept() #wait to accept a connection - blocking call
         #print('Connect Successfully\n' + str(addr))
-        #下面要multi thread
-        file_name_raw = client_socket.recv(16) #對應ljust(16)
+        #multi thread
+        file_name_raw = client_socket.recv(16) #ljust(16)
         file_name = str(file_name_raw.decode('utf-8'))
         #file_name = bytes(recvall(client_socket, 64)).decode('utf-8')
 
         cn += 1
         if(cn == 5):
-            service(client_socket, file_name, cn) #第5個client
+            service(client_socket, file_name, cn) 
         else:
             client_thread = threading.Thread(target = service, args = (client_socket, file_name, cn))
             client_thread.start()
